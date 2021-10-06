865445e5331b78712db881bd22440a0e42854288
Professional-Miles <mileslowe@pm.me>
2021-10-06 22:06:44 +0100

    Moved date formater test to DataHandlerTest, tested data read from cvs file rather than hardcoded data.

---
+++ src/test/java/com/spartaglobal/control/DataHandlerTest.java
@@ -0,0 +1,36 @@
+package com.spartaglobal.control;
+
+import com.spartaglobal.model.Employees;
+import org.junit.jupiter.api.Test;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Date;
+import java.text.ParseException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataHandlerTest {
+
+
+    @Test
+    void dateFormatSqlTest() {
+        String line = null;
+
+        try (BufferedReader in = new BufferedReader(new FileReader("EmployeeRecords.csv"))) {
+            String headerLine = in.readLine();
+            line = in.readLine();
+            Employees em = DataHandler.employeePopulate(line);
+            Date dob2 = Date.valueOf("1982-9-21");
+            assertEquals(dob2, em.getDateOfBirth());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
--- src/test/java/com/spartaglobal/model/EmployeesTest.java
+++ src/test/java/com/spartaglobal/model/EmployeesTest.java
@@ -1,21 +1,7 @@
 package com.spartaglobal.model;
 
-import org.junit.jupiter.api.Test;
-
-import java.sql.Date;
-import java.text.ParseException;
-
-import static com.spartaglobal.control.DataHandler.dateFormatSql;
-import static org.junit.jupiter.api.Assertions.*;
 
 class EmployeesTest {
 
-    @Test
-    public void dateFormatSqlTestPass() throws ParseException {
-        Employees em = new Employees();
-        String dob1 = "3/4/2000";
-        Date dob2 = Date.valueOf("2000-3-4");
-        assertEquals(dob2, dateFormatSql(dob1));
-    }
 
 }
