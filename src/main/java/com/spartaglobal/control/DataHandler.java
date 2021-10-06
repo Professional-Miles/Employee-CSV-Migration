ce2820708665fffcbb1df3bdffe5796a955a1714
Professional-Miles <mileslowe@pm.me>
2021-10-06 20:10:39 +0100

    Added the funtionality of populating a collection of Employee to the DataHandler class.

--- src/main/java/com/spartaglobal/control/DataHandler.java
+++ src/main/java/com/spartaglobal/control/DataHandler.java
@@ -1,8 +1,11 @@
 package com.spartaglobal.control;
 
+import com.spartaglobal.model.Employees;
+
 import java.sql.Date;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 
 public class DataHandler {
 
@@ -14,8 +17,14 @@ public class DataHandler {
 
     }
 
+    public static Employees employeePopulate (String data) throws ParseException {
 
+        String[] empList = data.split(",");
+        Employees emp = new Employees(Integer.valueOf(empList[0]),empList[1],empList[2],empList[3].charAt(0),empList[4],empList[5].charAt(0),
+                empList[6], dateFormatSql(empList[7]), dateFormatSql(empList[8]),Integer.valueOf(empList[9]));
 
+        return emp;
+    }
 
 
 
--- src/main/java/com/spartaglobal/reader/CSVReader.java
+++ src/main/java/com/spartaglobal/reader/CSVReader.java
@@ -1,9 +1,34 @@
 package com.spartaglobal.reader;
 
 
+import com.spartaglobal.control.DataHandler;
+import com.spartaglobal.model.Employees;
+
+import java.io.*;
+import java.text.ParseException;
+import java.util.ArrayList;
+
 public class CSVReader {
 
+    public static void main(String[] args) {
 
+        String line = null;
+        ArrayList<Employees> employeesList = new ArrayList<>();
+
+        try (BufferedReader in = new BufferedReader(new FileReader("EmployeeRecords.csv"));
+             BufferedWriter out = new BufferedWriter(new FileWriter("EmployeeOutput.txt"))) {
+            String headerLine = in.readLine();
+            while ((line = in.readLine()) != null){
+                employeesList.add(DataHandler.employeePopulate(line));
+            }
+
+        } catch (IOException | ParseException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println(employeesList);
+
+    }
 
 
 }
