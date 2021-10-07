be80140c45253bee153bf41dee3a3004ef805895
Professional-Miles <mileslowe@pm.me>
2021-10-07 01:38:09 +0100

    Added a data validation method to DataHandler, called this method before I populate the ArrayList of Employee.

--- src/main/java/com/spartaglobal/control/DataHandler.java
+++ src/main/java/com/spartaglobal/control/DataHandler.java
@@ -18,14 +18,43 @@ public class DataHandler {
 
     }
 
+    public static boolean isDataValid(String data) {
 
+        boolean validity = true;
+        String[] empList = data.split(",");
+        if (Integer.valueOf(empList[0]) < 0)
+            validity = false;
+        if (empList[1].length() > 5 || !empList[1].matches("[a-zA-Z]+\\."))
+            validity = false;
+        if (empList[2].length() > 256 || !empList[2].matches("\\p{L}+"))
+            validity = false;
+        if (!empList[3].substring(0,1).matches("[a-zA-Z]+"))
+            validity = false;
+        if (empList[4].length() > 256 || !empList[4].matches("\\p{L}+"))
+            validity = false;
+        if (!empList[5].matches("[FMfm]"))
+            validity = false;
+        if (!empList[6].matches("^(.+)@(.+)$"))
+            validity = false;
+        if (!empList[7].matches("(0?[1-9]|1[0-2])[\\/?$](0?[1-9]|[12]\\d|30|31)[\\/?$](\\d{4}|\\d{2})"))
+            validity = false;
+        if (!empList[8].matches("(0?[1-9]|1[0-2])[\\/?$](0?[1-9]|[12]\\d|30|31)[\\/?$](\\d{4}|\\d{2})"))
+            validity = false;
+        if (Integer.valueOf(empList[9]) < 0)
+            validity = false;
+
+        return validity;
+    }
 
     public static Employees employeePopulate (String data) throws ParseException {
 
         String[] empList = data.split(",");
-        Employees emp = new Employees(Integer.valueOf(empList[0]),empList[1],empList[2],empList[3].charAt(0),empList[4],empList[5].charAt(0),
-                empList[6], dateFormatSql(empList[7]), dateFormatSql(empList[8]),Integer.valueOf(empList[9]));
-
+        if (!isDataValid(data)) {
+            System.err.println("Data isn't valid!");
+            System.exit(1);
+        }
+        Employees emp = new Employees(Integer.valueOf(empList[0]), empList[1], empList[2], empList[3].charAt(0), empList[4], empList[5].charAt(0),
+                empList[6], dateFormatSql(empList[7]), dateFormatSql(empList[8]), Integer.valueOf(empList[9]));
         return emp;
     }
 
