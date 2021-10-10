47bd80ab9d2c0dde68174efecc9d404674129cd6
Professional-Miles <mileslowe@pm.me>
2021-10-10 23:50:21 +0100

    Finished logging.

--- src/main/java/com/spartaglobal/multithreading/Threading.java
+++ src/main/java/com/spartaglobal/multithreading/Threading.java
@@ -1,13 +1,15 @@
 package com.spartaglobal.multithreading;
 
 import com.spartaglobal.model.Employees;
-
+import org.apache.log4j.Logger;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 public class Threading {
+
+    private static final Logger log = Logger.getLogger("Threading logger.");
 
     public static void doThreading(String[] info, ArrayList<Employees> empList){
 
@@ -20,8 +22,10 @@ public class Threading {
         try {
            if (!service.awaitTermination(70, TimeUnit.SECONDS)){
                System.out.println("Threads timed out.");
+               log.warn("The multi thread writing timed out.");
            }
         } catch (InterruptedException e){
+            log.error("Interrupted Exception thrown.");
             e.printStackTrace();
         }
         long stopTime = System.currentTimeMillis();
